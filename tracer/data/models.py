00510d5a459b7b30bfc717118e4b28d4aa9164ce
aligoren <goren.ali@yandex.com>
2019-02-27 22:16:02 +0300

    Kullanıcı model'ini oluşturdum. Henüz migration gerçekleştirmedim. #1 fixes olur

---
+++ tracer/data/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
