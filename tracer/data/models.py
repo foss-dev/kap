b0afb92433813b8b59c685a0fefe58b2c42a8346
aligoren <goren.ali@yandex.com>
2019-02-27 22:18:41 +0300

    Unutulan init ve repr fonksiyonları eklendi

--- tracer/data/models.py
+++ tracer/data/models.py
@@ -7,3 +7,11 @@ class User(db.Model):
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
+
+    def __init__(self, email, password, active):
+        self.email = email
+        self.password = password
+        self.active = active
+    
+    def __repr__(self):
+        return '<User %r' % (self.email)
