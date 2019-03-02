5587f8804f0b7b921568f3fd9e3243e7f3f362b7
aligoren <goren.ali@yandex.com>
2019-03-02 19:34:20 +0300

    Models have User and Role classes now. They are connected with each other. Mixins are may not necessary here.

--- tracer/data/models.py
+++ tracer/data/models.py
@@ -1,17 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import UserMixin, RoleMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
+    roles = db.Column(db.Integer)
 
-    def __init__(self, email, password, active):
+    def __init__(self, email, password, active, roles):
         self.email = email
         self.password = password
         self.active = active
+        self.roles = roles
     
     def __repr__(self):
         return '<User %r' % (self.email)
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+    def __init__(self, name):
+
+        self.name = name
+
+    def __repr__(self):
+
+        return '<Role %r>' % (self.name)
