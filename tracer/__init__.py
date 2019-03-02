fbce1fcbfa94146ba07bde53e8f2e46ec0626a53
aligoren <goren.ali@yandex.com>
2019-03-02 19:32:19 +0300

    configure_app added for database operations. Db also initalize here

--- tracer/__init__.py
+++ tracer/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
 
+import config
+
 from tracer.api.user.controllers import user
+from tracer.data.models import db
+
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": ["*"]}})
+
+config.configure_app(app)
+db.init_app(app)
+
 
 app.register_blueprint(user, url_prefix="/api/user")
 
