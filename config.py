89d63f1df5fcfbb5d2253f3162f1a330addad88d
aligoren <goren.ali@yandex.com>
2019-03-02 19:30:36 +0300

    I added ENV config and Database file variable into the Development class. #5 Fixed

--- config.py
+++ config.py
@@ -1,3 +1,16 @@
+from tracer.data.models import db, User, Role
+
 class Development(object):
     PORT = 5000
     DEBUG = True
+    TESTING = False
+    ENV = 'dev'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///kap.db'
+
+
+config = {
+    "development": "config.Development"
+}
+
+def configure_app(app):
+    app.config.from_object(config['development'])
