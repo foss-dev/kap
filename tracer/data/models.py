dfaca0bd87bb4f246e22c1ddb975245f9bc450d7
aligoren <goren.ali@yandex.com>
2019-03-10 21:50:55 +0300

    I added Languages model for programming languages, ExceptionTypes model as a category and Logs model to store exceptions and queries

--- tracer/data/models.py
+++ tracer/data/models.py
@@ -42,7 +42,7 @@ class Keys(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     user_id = db.Column(db.Integer())
     application_id = db.Column(db.Integer())
-    key = db.Column(db.Integer()) # uuid.NAMESPACE_DNS, (current timestamp + application id + user id + secret key)
+    key = db.Column(db.String(60)) # uuid.NAMESPACE_DNS, (current timestamp + application id + user id + secret key)
     active = db.Column(db.Boolean(), default=True)
     created_at = db.Column(db.DateTime, default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
@@ -80,3 +80,56 @@ class Applications(db.Model):
     def __repr__(self):
 
         return '<Application %r>' % self.title
+
+class Languages(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))
+    logo_path = db.Column(db.String(150))
+
+    def __init__(self, name, logo_path):
+
+        self.name = name
+        self.logo_path = logo_path
+    
+    def __repr__(self):
+
+        return '<Language %r>' % self.name
+
+class ExceptionTypes(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text())
+    url = db.Column(db.String(255))
+
+    def __init__(self, name, description, url):
+
+        self.name = name
+        self.description = description
+        self.url = url
+    
+    def __repr__(self):
+
+        return '<Exception Type %r>' % self.name
+
+class Logs(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    language_id = db.Column(db.Integer())
+    exception_type_id = db.Column(db.Integer())
+    line_number = db.Column(db.Integer())
+    column_number = db.Column(db.Integer())
+    code = db.Column(db.Integer())
+    file_name = db.Column(db.String(150))
+    method_name = db.Column(db.String(150))
+    message = db.Column(db.Text())
+    trace = db.Column(db.String(255))
+    query = db.Column(db.Text())
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __init__(self, **kwargs):
+
+        super(Logs, self).__init__(**kwargs)
+    
+    def __repr__(self):
+
+        return '<Log %r>' % self.message
