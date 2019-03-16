cac264704c6911f831e1650ace14c30983192f43
aligoren <goren.ali@yandex.com>
2019-03-16 20:22:24 +0300

    I added update and new language add function to the languages layer. So, it fixed #22

--- tracer/api/languages/controllers.py
+++ tracer/api/languages/controllers.py
@@ -21,3 +21,79 @@ def all_list():
         })
 
     return jsonify(results)
+
+@language.route('/', methods=['POST'])
+def create_language():
+
+    status = {
+        "id": 0,
+        "success": False,
+        "message": "Error: Couldn't add language"
+    }
+
+    name = request.form['name']
+    logo_path = None
+
+    try:
+        logo_path = request.form['logo_path']
+    except Exception:
+        pass
+
+    new_language = Languages(name, logo_path)
+    db.session.add(new_language)
+
+    try:
+
+        db.session.commit()
+
+        if new_language.id > 0:
+            status = {
+                "id": new_language.id,
+                "success": True,
+                "message": "Language added successfully"
+            }
+
+    except Exception:
+
+        db.session.rollback()
+        status['message'] = "Unexpected error"
+
+
+    return jsonify(status)
+
+@language.route('/<int:id>', methods=['PUT'])
+def update_user(id):
+    
+    status = {
+        "id": 0,
+        "success": False,
+        "message": "Error: Couldn't update language"
+    }
+
+    name = request.json['name']
+    logo_path = None
+
+    try:
+        logo_path = request.json['logo_path']
+    except Exception:
+        pass
+
+    new_language = Languages.query.filter_by(id=id).first()
+
+    try:
+        
+        new_language.name = name
+        new_language.logo_path = logo_path
+
+        db.session.commit()
+
+        status['id'] = id
+        status['success'] = True
+        status['message'] = "Language updated successfully"
+
+    except AttributeError:
+
+        db.session.rollback()
+        status['message'] = "Language couldn't find"
+
+    return jsonify(status)
